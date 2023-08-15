21a23a96a9bdfdd23ce153d48340ddca7c8c68bb
KPX <dm.mikufans@gmail.com>
2023-08-16 00:07:03 +0800

    补文件…………

---
+++ 2236Utils/Commands/Test.fs
@@ -0,0 +1,26 @@
+﻿module KPX.AD2236.Commands.Test
+
+open System.IO
+open System.Text
+open System.Text.RegularExpressions
+
+open Newtonsoft.Json
+open Newtonsoft.Json.Linq
+
+open KPX.AD2236.Model
+
+
+let test projFile =
+
+    let ellipsis = Regex("\.{3,}")
+
+    let data =
+        let json = File.ReadAllText(projFile)
+
+        JArray.Parse(json).ToObject<ExportItem[]>()
+        |> Array.map (fun x ->
+            let final = x.Final.Replace("。」", "」")
+            let final = ellipsis.Replace(final, "……")
+            { x with Final = final })
+
+    File.WriteAllText(projFile, JsonConvert.SerializeObject(data, Formatting.Indented))
